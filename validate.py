9c593c1140cfd7644df9e47fc760fd8fc8407d26
LeCaleb <morganc@macstudent.com.au>
2019-05-27 08:45:52 +1000

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,6 +1,9 @@
+import re
+
 def ValidateEmail():
     Email = input("Enter Email: ")
     if "@" in email:
+        print(hi)
 
 def ValidateNumber():
 
@@ -8,12 +11,18 @@ def ValidateNumber():
 def ValidatePostcode():
 
 
-
 def ValidatePassword():
-
+    #Anthony and Caleb
+    password = input("Password: ")
+    characters = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    if password.islower() == False:
+        if (characters.search(password)) != None:
+            print("Password is valid")
+        else:
+            print("PROBLEM")
+    else:
+        print("Password is not Valid")
 
 
 def ValidateState():
-
-
 
