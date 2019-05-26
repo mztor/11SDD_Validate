1941d72550881b2a14bcc4ed85456549104dc7e2
Nilana <desilvan1@macstudent.com.au>
2019-05-27 08:42:20 +1000

    Update validate.py

--- validate.py
+++ validate.py
@@ -14,6 +14,20 @@ def ValidatePassword():
 
 
 def ValidateState():
-
+    State = input("What state do you live in? ")
+    if State == "NSW":
+        print("This is a valid state")
+    elif State == "VIC":
+        print("This is a valid state")
+    elif State == "NT":
+        print("This is a valid state")
+    elif State == "QLD":
+        print("This is a valid state")
+    elif State == "SA":
+        print("This is a valid state")
+    elif State == "WA":
+        print("This is a valid state")
+    elif State == "TAS":
+        print("This is a valid state")
 
 
