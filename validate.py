f0d0dd4d3780a8ca2f2cfcfa30884ebe0d4cb441
Josh <draytonj@macstudent.com.au>
2019-05-27 08:26:13 +1000

    Code
    Code

--- validate.py
+++ validate.py
@@ -1,6 +1,18 @@
 def ValidateEmail():
-    Email = input("Enter Email: ")
+    email = input("Enter Email: ")
     if "@" in email:
+        if "." in email:
+            pos1 = int(email.find("@"))
+            pos2 = int(email.find("."))
+            if pos1 < pos2:
+                print("Your email is valid!")
+            else:
+                print("Your email is invalid!")
+        else:
+            print("Your email is invalid!")
+    else:
+        print("Your email is invalid!")
+
 
 def ValidateNumber():
 
