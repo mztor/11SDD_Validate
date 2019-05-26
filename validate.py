c5ef034d3e0cfe8974e533ccc9343f323f06cb3e
Nilana <desilvan1@macstudent.com.au>
2019-05-27 08:58:17 +1000

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,18 +1,42 @@
+#JOSHUA DRAYTON
+#MAKES SURE THAT EMAIL IS VALID, WITH ONE @ SYMBOL, AT LEAST ONE ., AND THE @ BEFORE THE ..
 def ValidateEmail():
-    Email = input("Enter Email: ")
-    if "@" in email:
+    email = input("Enter Email: ")
+    amount = int(email.count("@"))
+    if amount == 1:
+        if "." in email:
+            pos1 = int(email.find("@"))
+            pos2 = int(email.find("."))
+            if pos1 < pos2:
+                print("Your email is valid!")
+            else:
+                print("Your email is invalid!")
+        else:
+            print("Your email is invalid!")
+    else:
+        print("Your email is invalid!")
+
 
 def ValidateNumber():
+    print("Stub")
 
 
+#Jayden Taylor
+#Makes sure the postcode is four numbers, as per Australian standards
 def ValidatePostcode():
-
-
+    postcode = input("What is your postcode? ")
+    if postcode.len() != 4:
+        print("Your postcode is invalid!")
+        return 1
+    else:
+        print("Your postcode is valid!")
+        return 0
 
 def ValidatePassword():
+    print("stub")
 
 
-
+#Nilanas code, validate Australain states
 def ValidateState():
     State = input("What state do you live in? ")
     if State == "NSW":
@@ -29,5 +53,8 @@ def ValidateState():
         print("This is a valid state")
     elif State == "TAS":
         print("This is a valid state")
+
+
+
 
 
